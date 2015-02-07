d4f48453a19d55d8772acac074f2930327a38caf
xabita <mutskxabaa@gmail.com>
2015-02-06 19:44:34 -0600

    Practica003
    Selecciona Imagen, Video y Los muestra en lista

---
+++ AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  Practica003
+//
+//  Created by Elizabeth Martínez Gómez on 06/02/15.
+//  Copyright (c) 2015 Elizabeth Martínez Gómez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+int iCont;
+int i;
+
+NSMutableArray *maNames;
+NSMutableArray *maImgs;
+NSMutableArray *maEstatus;
+NSMutableArray *maUrl;
+NSString *indexTemp;
+NSString *valNombre;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Practica003
+//
+//  Created by Elizabeth Martínez Gómez on 06/02/15.
+//  Copyright (c) 2015 Elizabeth Martínez Gómez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIButton *btnAgregar;
+@property (strong, nonatomic) IBOutlet UIButton *btnConsultar;
+
+@end
+
